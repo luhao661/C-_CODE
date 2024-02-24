b72cb35e02a95e23a08abf48635a473beab53f20
luhao661 <102971130+luhao661@users.noreply.github.com>
2024-02-24 21:12:13 +0800

    Add files via upload

--- Source_code_integration/CPP_Standard_Library/CPP_Standard_Library_code.cpp
+++ Source_code_integration/CPP_Standard_Library/CPP_Standard_Library_code.cpp
@@ -5771,22 +5771,26 @@ using namespace std;
 
 void doSomething(int num, char c)
 {
-    try {
+    try
+    {
         // random-number generator (use c as seed to get different sequences)
         default_random_engine dre(42 * c);
         uniform_int_distribution<int> id(10, 1000);
-        for (int i = 0; i < num; ++i) {
+        for (int i = 0; i < num; ++i)
+        {
             this_thread::sleep_for(chrono::milliseconds(id(dre)));
             cout.put(c).flush();
             //...
         }
     }
     // make sure no exception leaves the thread and terminates the program
-    catch (const exception& e) {
+    catch (const exception& e)
+    {
         cerr << "THREAD-EXCEPTION (thread "
             << this_thread::get_id() << "): " << e.what() << endl;
     }
-    catch (...) {
+    catch (...)
+    {
         cerr << "THREAD-EXCEPTION (thread "
             << this_thread::get_id() << ")" << endl;
     }
@@ -5794,16 +5798,20 @@ void doSomething(int num, char c)
 
 int main()
 {
-    try {
+    try
+    {
         thread t1(doSomething, 5, '.');  // print five dots in separate thread
         cout << "- started fg thread " << t1.get_id() << endl;
+        //t1线程负责打印 "....."
 
         // print other characters in other background threads
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 5; ++i)
+        {
             thread t(doSomething, 10, 'a' + i); // print 10 chars in separate thread
             cout << "- detach started bg thread " << t.get_id() << endl;
             t.detach();  // detach thread into the background
         }
+        //5个线程负责打印a到e共50个字母
 
         cin.get();  // wait for any input (return)
 
